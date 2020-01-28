17a5fb8f143f92e9fe64429305519ff168a03762
fulton <gprlabs@outlook.com>
2020-01-28 08:47:26 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var http = require("http");
+var stocksClass = require("./models/stocks")
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    let url = request.url
+    if(url.startsWith("/stocks")) response.end(stocksClass.);
+    
+    // Send the response body as "Hello World"
+ }).listen(8081);
+ 
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
---
+++ models/stocks.js
@@ -0,0 +1,10 @@
+
+class stock {
+    constructor(name, sym, price, diffOnDayParam) {
+        this.name = name;
+        this.symbol = sym;
+        this.price = price;
+        this.diffOnDay = diffOnDayParam;
+    }
+}
+
