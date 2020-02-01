6b6566929f27eb9c9fc10ad5c15576a2426d89ae
fulton <gprlabs@outlook.com>
2020-02-01 13:12:55 -0800

    working on the api

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     let url = request.url
     let array =new Array()
-    array.push(new stocksClass("", "", "", ""))
+    array.push(new stocksClass("hrfdh", "", "", ""))
     let data = new Buffer(array)
-    if(url.startsWith("/stocks")) response.end(data.toJSON)
+    if(url.startsWith("/stocks")) response.end(data.toJSON.toString
+        
+        
+        
+        
+        
+        )
     
     // Send the response body as "Hello World"
  }).listen(8081);
