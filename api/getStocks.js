23fd6d09d4d7395b6cc5d9722b84e11b1393b6af
fulton <gprlabs@outlook.com>
2020-02-02 19:38:56 -0800

    getting some where

--- api/getStocks.js
+++ api/getStocks.js
@@ -1,3 +1,4 @@
+module.exports = 
 function getStocks(){
-    
+    return "hi"
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var http = require("http");
+var getStocks = require("./api/getStocks")
 var stocksClass = require("./models/stocks")
 http.createServer(function (request, response) {
     // Send the HTTP header 
@@ -7,13 +8,11 @@ http.createServer(function (request, response) {
     // Content Type: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
     let url = request.url
-    let array =new Array()
-    array.push(new stocksClass("hrfdh", "", "", ""))
-    let data = new Buffer(array)
-    if(url.startsWith("/stocks")) response.end(data.toJSON.toString)
+    if(url.startsWith("/stocks")) response.end(getStocks())
     
     // Send the response body as "Hello World"
  }).listen(8081);
  
+
  // Console will print the message
  console.log('Server running at http://127.0.0.1:8081/');
