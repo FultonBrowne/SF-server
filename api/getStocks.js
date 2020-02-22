48fbcc787ce1a2dbde538bb65633af3dd4726a84
fulton <gprlabs@outlook.com>
2020-02-21 19:13:02 -0800

    working on api

--- api/getStocks.js
+++ api/getStocks.js
@@ -11,36 +11,20 @@ type Query {
 },
 type Course {
     id: Int
-    title: String
-    author: String
-    description: String
-    topic: String
+    sym: String
+    price: Int
+    priceToday: Int
+    priceYesterday: Int
     url: String
 }
 `);var coursesData = [
 {
     id: 1,
-    title: 'The Complete Node.js Developer Course',
-    author: 'Andrew Mead, Rob Percival',
-    description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-    topic: 'Node.js',
+    sym: 'The Complete Node.js Developer Course',
+    price: 0,
+    priceToday: 0,
+    priceYesterday: 0,
     url: 'https://codingthesmartway.com/courses/nodejs/'
-},
-{
-    id: 2,
-    title: 'Node.js, Express & MongoDB Dev to Deployment',
-    author: 'Brad Traversy',
-    description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-    topic: 'Node.js',
-    url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-},
-{
-    id: 3,
-    title: 'JavaScript: Understanding The Weird Parts',
-    author: 'Anthony Alicea',
-    description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-    topic: 'JavaScript',
-    url: 'https://codingthesmartway.com/courses/understand-javascript/'
 }
 ]
 var getCourse = function(args) { 
@@ -49,7 +33,7 @@ return coursesData.filter(course => {
     return course.id == id;
 })[0];
 }
-var getCourses = function(args) {
+var getStocks = function(args) {
 if (args.topic) {
     var topic = args.topic;
     return coursesData.filter(course => course.topic === topic);
@@ -59,7 +43,7 @@ if (args.topic) {
 }
 var root = {
 course: getCourse,
-courses: getCourses
+courses: getStocks
 };// Create an express server and a GraphQL endpoint
 app.use('/graphql', express_graphql({
 schema: schema,
