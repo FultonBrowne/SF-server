db03600756dafcb7749fe5c3bc3a79a3f635c044
fulton <gprlabs@outlook.com>
2020-02-29 08:37:13 -0800

    working on the cache

--- api/stockCache.js
+++ api/stockCache.js
@@ -9,44 +9,24 @@ module.exports =
         getBasicData(element)
       });
 }
-function getURL(url){
-  var toReturn
-  var http = require('https');
-
+function getURL(url, fun){
+  let toReturn = ""
+  var request = require('request');
   var options = {
-    url : url
+    'method': 'GET',
+    'url': url,
+    'headers': {
+    }
   };
-  http.get(url)
-  var req = http.get(options, function(res) {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
+  request(options, fun);
   
-    // Buffer the body entirely for processing as a whole.
-    var bodyChunks = [];
-    res.on('data', function(chunk) {
-      // You can process streamed parts here...
-      bodyChunks.push(chunk);
-    }).on('end', function() {
-      var body = Buffer.concat(bodyChunks);
-      console.log('BODY: ' + body);
-      toReturn = body
-    })
-  });
-  
-  req.on('error', function(e) {
-    console.log('ERROR: ' + e.message);
-  });
-  return toReturn
 }
 function getBasicData(sym){
   let url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo"
-  let data = getURL(url)
-  console.log(data)
-  let obj = JSON.parse(data)
+  let fun = function (error, response){
+    console.log(response.body)
+  }
+  getURL(url, fun)
+  console.log()
 
-console.log(obj.count);
-// expected output: 42
-
-console.log(obj.result);
-// expected output: true
 }
