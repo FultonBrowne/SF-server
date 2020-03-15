7b3371cd974bc8289912f7cf02a25fb43bddb192
fulton <gprlabs@outlook.com>
2020-03-15 14:14:21 -0700

    refactoring

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
-var http = require("http");
 var express = require('express');
-var getStocks = require("./api/getStocks")
 var stocksCache = require("./api/stockCache")
 var app = express();
 new stocksCache()
-let cache = new getStocks(app)
  // Console will print the message
  app.listen(8080, () => console.log('Express GraphQL Server Now Running On localhost:8080/graphql'));
 
